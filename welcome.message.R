4deb6ca1383a0e064eff38633fcda1d8959a660f
Eric Wan <ericwan@Erics-MacBook-Pro.local>
2020-07-30 13:03:27 -0500

    project update

--- welcome.message.R
+++ welcome.message.R
@@ -1,7 +1,6 @@
 population = 7000000000
 countries = 195
-string = "Feel free to visit the
-    White House and pick up your complimentary visitor badge and gift bag." 
+string = "Feel free to visit the White House and pick up your complimentary visitor badge and gift bag." 
 
 cat("Hello, welcome to Earth, the third planet from the Sun! There are over"
     ,population,"sentient humans who live on this planet. We belong to",countries,
