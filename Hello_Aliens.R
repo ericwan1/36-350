c1bd5579e4ac43891c4c2ad31ed9519cac35d0ed
Eric Wan <ericwan@Erics-MacBook-Pro.local>
2020-07-30 18:15:32 -0500

    Last Problem

---
+++ Hello_Aliens.R
@@ -0,0 +1,7 @@
+population = 7000000000
+countries = 195
+string = "Feel free to visit the White House and pick up your complimentary gift bag." 
+
+cat("Hello, welcome to Earth, the third planet from the Sun! There are over"
+    ,population,"sentient humans who live on this planet. We belong to",countries,
+    "different countries but live together on one planet.",string,fill=70)
